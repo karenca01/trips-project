524d452100b9101892b49439ced47c07b76279d3
karenca01 <kcervantes01@alumnos.uaq.mx>
2025-11-27 10:35:35 -0600

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(); //en producción
+
+  // app.enableCors({
+  //   origin: 'donde corre el front',
+  //   credentials: true,
+  // })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
